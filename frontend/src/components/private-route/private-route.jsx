cee200a37a2f34fb188760c683f00419822d083e
Sergey <skauzerlfc@gmail.com>
2025-02-06 22:23:54 +0300

    Исправление приватного роута

--- frontend/src/components/private-route/private-route.jsx
+++ frontend/src/components/private-route/private-route.jsx
@@ -4,13 +4,13 @@ import { selectUserId } from '../../selectors';
 
 export const PrivateRoute = ({ children }) => {
 	// Получаем userId из Redux store
+	const user = sessionStorage.getItem('userData');
 	const userId = useSelector(selectUserId);
-
 	// Если нет данных о пользователе, перенаправляем на страницу логина
 	if (userId === null) {
 		// Если загрузка данных ещё не завершена, можно показать индикатор загрузки
 		return;
 	}
 
-	return userId ? children : <Navigate to="/login" />;
+	return user ? children : <Navigate to="/login" />;
 };
